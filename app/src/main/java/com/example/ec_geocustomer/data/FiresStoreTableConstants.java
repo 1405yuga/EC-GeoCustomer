809268bcf5d3ef12149190a3f89c80ce5cfe64dd
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-02-17 16:35:29 +0530

    constants file added

---
+++ app/src/main/java/com/example/ec_geocustomer/data/FiresStoreTableConstants.java
@@ -0,0 +1,74 @@
+package com.example.ec_geocustomer.data;
+
+public class FiresStoreTableConstants {
+    final String barcode = "Barcode";
+    final String customer = "Customer";
+    final String owner = "Owner";
+
+    //to fetch from barcode database
+    final String barcodeName = "Name";
+    final String barcodeCategory = "Category";
+    final String barcodeSubCatgeory = "Sub-category";
+    final String barcodeBrand = "Brand";
+    final String barcodePrice = "Price";
+    final String barcodeSize = "size";
+    final String barcodeUrl = "url";
+
+
+    //owner database
+    final String ownerAvailability = "Availability";
+    final String ownerDiscount="discount";
+    final String ownerQuantity="quantity";
+
+    public String getBarcodeUrl() {
+        return barcodeUrl;
+    }
+
+    public String getOwnerDiscount() {
+        return ownerDiscount;
+    }
+
+    public String getOwnerQuantity() {
+        return ownerQuantity;
+    }
+
+    public String getOwnerAvailability() {
+        return ownerAvailability;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getBarcodeName() {
+        return barcodeName;
+    }
+
+    public String getBarcodeCategory() {
+        return barcodeCategory;
+    }
+
+    public String getBarcodeSubCatgeory() {
+        return barcodeSubCatgeory;
+    }
+
+    public String getBarcodeBrand() {
+        return barcodeBrand;
+    }
+
+    public String getBarcodePrice() {
+        return barcodePrice;
+    }
+
+    public String getBarcodeSize() {
+        return barcodeSize;
+    }
+}
