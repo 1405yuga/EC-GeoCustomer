4a4b0e1f4a86f25e98a45956cfd9eea77f382b28
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-02-17 16:34:43 +0530

    firestore data connected to suggestions in searchview

--- app/src/main/java/com/example/ec_geocustomer/SearchViewFragment.java
+++ app/src/main/java/com/example/ec_geocustomer/SearchViewFragment.java
@@ -1,7 +1,5 @@
 package com.example.ec_geocustomer;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
@@ -13,17 +11,13 @@ import android.annotation.SuppressLint;
 import android.app.SearchManager;
 import android.database.Cursor;
 import android.database.MatrixCursor;
-import android.location.Address;
-import android.location.Geocoder;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.provider.BaseColumns;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FilterQueryProvider;
 
+import com.example.ec_geocustomer.data.FiresStoreTableConstants;
 import com.example.ec_geocustomer.databinding.FragmentSearchViewBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -31,25 +25,28 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 
 
 public class SearchViewFragment extends Fragment {
 
     FragmentSearchViewBinding binding;
-    boolean newPoints=false;
+    boolean newPoints = false;
     ArrayList<LatLng> locationArrayList = new ArrayList<>();
     LatLng sydney = new LatLng(-34, 151);
     LatLng TamWorth = new LatLng(-31.083332, 150.916672);
     LatLng NewCastle = new LatLng(-32.916668, 151.750000);
     LatLng Brisbane = new LatLng(-27.470125, 153.021072);
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+    List<String> list = new ArrayList<>();
+    SimpleCursorAdapter cursorAdapter;
 
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
 
         /**
@@ -64,13 +61,12 @@ public class SearchViewFragment extends Fragment {
         @Override
         public void onMapReady(GoogleMap googleMap) {
 
-            if(newPoints){
-                for(int i=0;i<locationArrayList.size();i++){
+            if (newPoints) {
+                for (int i = 0; i < locationArrayList.size(); i++) {
                     googleMap.addMarker(new MarkerOptions().position(locationArrayList.get(i)).title("Marker"));
                     googleMap.moveCamera(CameraUpdateFactory.newLatLng(locationArrayList.get(i)));
                 }
-            }
-            else{
+            } else {
                 LatLng sydney = new LatLng(-34, 151);
                 googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
                 googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
@@ -78,14 +74,13 @@ public class SearchViewFragment extends Fragment {
         }
     };
 
-    List<String>list = new ArrayList<>();
-    SimpleCursorAdapter cursorAdapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        binding= FragmentSearchViewBinding.bind(inflater.inflate(R.layout.fragment_search_view, container, false));
+        binding = FragmentSearchViewBinding.bind(inflater.inflate(R.layout.fragment_search_view, container, false));
         /*
         binding.btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -106,14 +101,27 @@ public class SearchViewFragment extends Fragment {
         list.add("a");
         list.add("ab");
         list.add("ab c");
-        int [] to =new int[] {R.id.searchItemID};
-        String[] from=new String[]{SearchManager.SUGGEST_COLUMN_TEXT_1};
 
-        cursorAdapter=new SimpleCursorAdapter(getActivity(),R.layout.suggestion_list,null,from,to,CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        // TODO: 17-02-2023 get itemnames
+        FirebaseFirestore firebaseFirestore=FirebaseFirestore.getInstance();
+        FiresStoreTableConstants constants=new FiresStoreTableConstants();
+        firebaseFirestore.collection(constants.getBarcode())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<DocumentSnapshot> documents=queryDocumentSnapshots.getDocuments();
+                        for(DocumentSnapshot documentSnapshot :documents){
+                            list.add(documentSnapshot.getString(constants.getBarcodeName()));
+                        }
+
+                    }
+                });
+
+        int[] to = new int[]{R.id.searchItemID};
+        String[] from = new String[]{SearchManager.SUGGEST_COLUMN_TEXT_1};
+        cursorAdapter = new SimpleCursorAdapter(getActivity(), R.layout.suggestion_list, null, from, to, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
         binding.searchView.setSuggestionsAdapter(cursorAdapter);
-
-
-
         binding.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -123,11 +131,11 @@ public class SearchViewFragment extends Fragment {
                 locationArrayList.add(TamWorth);
                 locationArrayList.add(NewCastle);
                 locationArrayList.add(Brisbane);
-                newPoints=true;
+                newPoints = true;
 
 
                 binding.searchView.clearFocus();
-                onViewCreated(binding.getRoot(),savedInstanceState);
+                onViewCreated(binding.getRoot(), savedInstanceState);
 
                 return true;
             }
@@ -135,12 +143,12 @@ public class SearchViewFragment extends Fragment {
             @Override
             public boolean onQueryTextChange(String newText) {
                 // TODO: 28-01-2023 suggestions
-                MatrixCursor cursor=new MatrixCursor(new String[]{
-                        BaseColumns._ID,SearchManager.SUGGEST_COLUMN_TEXT_1
+                MatrixCursor cursor = new MatrixCursor(new String[]{
+                        BaseColumns._ID, SearchManager.SUGGEST_COLUMN_TEXT_1
                 });
-                for (int j=0; j<list.size(); j++) {
+                for (int j = 0; j < list.size(); j++) {
                     if (list.get(j).toLowerCase().startsWith(newText.toLowerCase()))
-                        cursor.addRow(new Object[] {j, list.get(j)});
+                        cursor.addRow(new Object[]{j, list.get(j)});
                 }
                 cursorAdapter.changeCursor(cursor);
                 return false;
@@ -155,9 +163,9 @@ public class SearchViewFragment extends Fragment {
 
             @Override
             public boolean onSuggestionClick(int position) {
-                Cursor cursor1=(Cursor) cursorAdapter.getItem(position);
-                @SuppressLint("Range") String item=cursor1.getString(cursor1.getColumnIndex(SearchManager.SUGGEST_COLUMN_TEXT_1));
-                binding.searchView.setQuery(item,true);
+                Cursor cursor1 = (Cursor) cursorAdapter.getItem(position);
+                @SuppressLint("Range") String item = cursor1.getString(cursor1.getColumnIndex(SearchManager.SUGGEST_COLUMN_TEXT_1));
+                binding.searchView.setQuery(item, true);
                 return true;
             }
         });
@@ -167,7 +175,7 @@ public class SearchViewFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding=null;
+        binding = null;
     }
 
     @Override
