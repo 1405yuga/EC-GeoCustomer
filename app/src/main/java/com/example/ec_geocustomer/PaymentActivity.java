43c20ffb00a1e07b43c0e18370800fedb636f5f8
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-03-13 13:15:18 +0530

    payment prototype implemented

--- app/src/main/java/com/example/ec_geocustomer/PaymentActivity.java
+++ app/src/main/java/com/example/ec_geocustomer/PaymentActivity.java
@@ -2,25 +2,60 @@ package com.example.ec_geocustomer;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.ec_geocustomer.data.FiresStoreTableConstants;
 import com.example.ec_geocustomer.data.ItemBarcode;
 import com.example.ec_geocustomer.data.Shop;
 import com.example.ec_geocustomer.databinding.ActivityPaymentBinding;
+import com.example.ec_geocustomer.databinding.RecommendDialogBinding;
+import com.example.ec_geocustomer.recommendation.RecommendationAdapter;
+import com.example.ec_geocustomer.recommendation.RecommendationData;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import dev.shreyaspatil.easyupipayment.EasyUpiPayment;
+import dev.shreyaspatil.easyupipayment.exception.AppNotFoundException;
+import dev.shreyaspatil.easyupipayment.listener.PaymentStatusListener;
+import dev.shreyaspatil.easyupipayment.model.PaymentApp;
+import dev.shreyaspatil.easyupipayment.model.TransactionDetails;
 
 public class PaymentActivity extends AppCompatActivity {
 
     ActivityPaymentBinding binding;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore=FirebaseFirestore.getInstance();
+
+    FiresStoreTableConstants constants=new FiresStoreTableConstants();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding=ActivityPaymentBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        firebaseAuth=FirebaseAuth.getInstance();
         Shop shop= (Shop) getIntent().getSerializableExtra("shop");
         ItemBarcode itemBarcode= (ItemBarcode) getIntent().getSerializableExtra("itembarcode");
         Double d=itemBarcode.getMrp();
@@ -45,11 +80,46 @@ public class PaymentActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // TODO: 12-03-2023 integrate payment gateway
+                Date c = Calendar.getInstance().getTime();
+                SimpleDateFormat df = new SimpleDateFormat("ddMMyyyyHHmmss", Locale.getDefault());
+                String transcId = df.format(c);
+                try {
+                    EasyUpiPayment.Builder builder = new EasyUpiPayment.Builder(PaymentActivity.this)
+                            .setPayeeVpa("example@vpa")
+                            .setPayeeName("yuga")
+                            .setPayeeMerchantCode("1234")
+                            .setTransactionId(transcId)
+                            .setTransactionRefId(transcId)
+                            .setDescription(itemBarcode.getName())
+                            .setAmount(total.toString());
+
+                    EasyUpiPayment easyUpiPayment = builder.build();
+                    easyUpiPayment.startPayment();
+                    easyUpiPayment.setPaymentStatusListener(new PaymentStatusListener() {
+                        @Override
+                        public void onTransactionCompleted(@NonNull TransactionDetails transactionDetails) {
+                            Toast.makeText(PaymentActivity.this, "Ordered placed successfully !", Toast.LENGTH_LONG).show();
+                            Log.d(TAG,"transaction completed");
+                        }
+
+                        @Override
+                        public void onTransactionCancelled() {
+                            Toast.makeText(PaymentActivity.this, "Transaction Cancelled!", Toast.LENGTH_LONG).show();
+                            Log.d(TAG,"transaction cancelled");
+                        }
+                    });
+                } catch (AppNotFoundException e) {
+                    Toast.makeText(PaymentActivity.this, "You don't have UPI application for payment !", Toast.LENGTH_SHORT).show();
+                    e.getStackTrace();
+                }
+
+
+
 
             }
         });
 
 
-
     }
+
 }
